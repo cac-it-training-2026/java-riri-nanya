0bbe046bd18c760941d7bbc4bab9cba1ac37ffc3
ririkana <ririkana@C010423486.zipangu.cac.co.jp>
2026-04-16 14:48:36 +0900

    完了

--- java_comprehension_exercises_volume1/src/basic/question02/Question02_1.java
+++ java_comprehension_exercises_volume1/src/basic/question02/Question02_1.java
@@ -9,8 +9,14 @@ import java.io.IOException;
 public class Question02_1 {
 
 	public static void main(String[] args) throws IOException {
+		System.out.println("名前を入力してください。");
 
-		// TODO:ここに実装
+		String name = "システム太郎";
+
+		System.out.println("好きなスポーツを入力してください。");
+		String sport = "サッカー";
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("好きなスポーツは" + sport + "です。");
 
 	}
 }
--- java_comprehension_exercises_volume1/src/basic/question03/Question03_2.java
+++ java_comprehension_exercises_volume1/src/basic/question03/Question03_2.java
@@ -8,7 +8,19 @@ public class Question03_2 {
 
 	public static void main(String[] args) {
 
-		// TODO:ここに実装
-		
+		int basePrice = 1200;
+		int ticket = 3;
+		int reducedPrice = (basePrice - 200);
+		int readucedTickets = ticket;
+		readucedTickets = readucedTickets--;
+		int totalPrice = reducedPrice * readucedTickets;
+		double taxRate = 1.1;
+		int totalWithTax = (int) (totalPrice * taxRate);
+
+		System.out.println("購入可能枚数は" + readucedTickets + "枚です。");
+		System.out.println("値下げごの１枚あたりの料金は" + reducedPrice + "円です。");
+		System.out.println("合計金額（税抜）は " + totalPrice + "です。");
+		System.out.println("合計金額（税抜）は " + totalWithTax + "です。");
+
 	}
 }
